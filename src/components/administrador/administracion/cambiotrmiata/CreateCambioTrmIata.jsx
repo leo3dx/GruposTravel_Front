076913e9e6c68000f4c49eb75c6ccc76fae1717a
leo3dx <leo3dxl@gmail.com>
2022-07-12 20:43:31 -0500

    Actualización de ramas

--- src/components/administrador/administracion/cambiotrmiata/CreateCambioTrmIata.jsx
+++ src/components/administrador/administracion/cambiotrmiata/CreateCambioTrmIata.jsx
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import {useNavigate} from 'react-router-dom';
+import {FormControlLabel, Switch} from '@mui/material'
 
 const CreateCnfMoneda = () => {
 
     const history = useNavigate();
+    const [checked, setChecked] = useState(true);
+
+    const switchHandler = (e) => {
+        setChecked(e.target.checked);
+      }
 
     const guardar = (e) => {
 
         e.preventDefault();
         
         let data = {
-            nombre_moneda : document.getElementById('nombre_moneda').value,
-            estado_moneda : Number(document.getElementById('estado_moneda').value)
-        }
-        axios.post(process.env.REACT_APP_API_HOST + 'cnfMoneda',data)
+            nombre_moneda: document.getElementById("nombre_moneda").value,
+            tipo_factor: document.getElementById("tipo_factor").value,
+            valor_cambio: document.getElementById("valor_cambio").value,
+            estado_cambio: checked,
+          };
+
+        axios.post(process.env.REACT_APP_API_HOST + 'cnfFactorCambio',data)
         .then(response => {
             alert('Registro guardado')
-            history('/administrador/cnfmoneda')
+            history('/administrador/cambiotrmiata')
         }).catch(err => {
             console.log(err);
         })
@@ -26,24 +35,55 @@ const CreateCnfMoneda = () => {
 
     return(
         <>
-            <div className="p-2 btn-1 fs-5 text-center">Crear tipo de moneda</div>
-            <form className="form-control pt-3" onSubmit={guardar}>
-                <div className="row">
-                    <div className="col">
-                        <label className="form-label">Tipo de moneda</label>
-                        <input type="text" id='nombre_moneda' required className="form-control"/>
-                    </div>
-                    <div className="col">
-                        <label className="form-label">Estado</label>
-                        <select id="estado_moneda" required className="form-select">
-                            <option value="1">Activo</option>
-                            <option value="0">Inactivo</option>
-                        </select>
-                    </div>
-                </div>
-                <input type="submit" className='btn btn-1 mt-3' value='Guardar'/>
-            </form>
-        </>
+      <div className="p-2 btn-1 text-center">Editar tipo de Cambio</div>
+      
+      <form className="form-control pt-3" onSubmit={guardar}>
+        <div className="row">
+          <div className="row">
+            <div className="col">
+              <label className="form-label">Moneda</label>
+              <input
+                type="text"
+                id="nombre_moneda"
+                className="form-control"
+                defaultValue='Dolar'
+              />
+            </div>
+            <div className="col">
+              <label className="form-label">Tipo de factor</label>
+              <select id="tipo_factor" className="form-select">
+                <option value="TRM">TRM</option>
+                <option value="IATA">IATA</option>
+              </select>
+            </div>
+          </div>
+          <div className="row mt-3">
+            <div className="col">
+              <label className="form-label">Valor</label>
+              <input
+                type="number"
+                id="valor_cambio"
+                className="form-control"
+                defaultValue='1000'
+              />
+            </div>
+            <div className="col d-flex flex-column justify-content-center">
+              <label className="form-label">Estado</label>
+              <FormControlLabel
+                id="estado_cambio"
+                control={<Switch checked={checked} onChange={switchHandler}/>}
+                label={
+                    checked
+                    ? "Activo"
+                    : "Inactivo"
+                }
+              />
+            </div>
+          </div>
+        </div>
+        <input type="submit" className="btn btn-1 mt-3" value="Guardar" />
+      </form>
+    </>
     )
 
 }
--- src/components/administrador/administracion/cambiotrmiata/EditCambioTrmIata.jsx
+++ src/components/administrador/administracion/cambiotrmiata/EditCambioTrmIata.jsx
@@ -1,102 +1,147 @@
-import React, { useState, useEffect } from 'react';
-import {useParams, useNavigate} from 'react-router-dom';
-import logo  from '../../../assets/img/logo.jpeg';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate, useLocation } from "react-router-dom";
+import LinearProgress from "@mui/material/LinearProgress";
+import { FormControlLabel, Switch } from "@mui/material";
+import logo from "../../../../assets/img/logo.jpeg";
+import axios from "axios";
 
-const EditCnfMoneda = () => {
+const EditCambioTrmIata = () => {
+  const history = useNavigate();
+  let location = useLocation();
+  const params = useParams();
+  const [factorCambio, setFactorCambio] = useState([]);
+  const [loading, setLoading] = useState([true]);
+  const [checked, setChecked] = useState(true);
 
-    const history = useNavigate();
-    const params = useParams();
-    const [cnfMoneda, setCnfMoneda] = useState([]);
-    const [loading, setLoading] = useState([true]);
-
-    useEffect(() => {
-
-        axios.get(process.env.REACT_APP_API_HOST + 'cnfMoneda')
-        .then((response) => {
-            let result = response.data.filter( moneda => moneda.ID_MONEDA === Number(params.id));
-            setCnfMoneda(result);
-            setLoading(false);
-        }).catch((error) => {
-            console.log(error);
-        })
-
-    },[]);
-
-    const guardar = (e) => {
-        
-        e.preventDefault();
-        let data = {
-            nombre_moneda : document.getElementById('nombre_moneda').value,
-            estado_moneda : Number(document.getElementById('estado_moneda').value)
-        }
-        axios.put(process.env.REACT_APP_API_HOST + 'cnfMoneda/' + params.id, data)
-        .then((response) => {
-            alert('Registro actualizado')
-            history('/administrador/cnfMoneda')
-        }).catch((error => {
-            console.log(error);
-        }))
-
-    }
-
-    if(loading){
-        return(
-            <>
-                <div className='text-center'>
-                    <div>
-                        <h2>Cargando</h2>
-                    </div>
-                    <div>
-                        <img src={logo} width="300" alt="" />
-                    </div>
-                </div>
-            </>
+  useEffect(() => {
+    axios
+      .get(process.env.REACT_APP_API_HOST + "cnfFactorCambio")
+      .then((response) => {
+        let result = response.data.filter(
+          (cambio) => cambio.ID_FACTOR === Number(params.id)
         );
-    }
+        setFactorCambio(result);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    return(
-        <>
-            <div className="p-2 btn-1 text-center">Editar tipo de moenda</div>
-            <form className="form-control pt-3" onSubmit={guardar}>
-                <div className="row">
+  const switchHandler = (e) => {
+    setChecked(e.target.checked);
+  }
 
-                    {
-                        cnfMoneda.map((moneda) => (
-                            <>
-                                <div className="col">
-                                    <label className="form-label">Tipo de moneda</label>
-                                    <input type="text" id='nombre_moneda' className="form-control" defaultValue={moneda.NOMBRE_MONEDA}/>
-                                </div>
-                                <div className="col">
-                                    <label className="form-label">Estado</label>
-                                    <select id="estado_moneda" className="form-select">
-                                        {
-                                            moneda.ESTADO_MONEDA? (
-                                                    <>
-                                                        <option value="1">Activo</option>
-                                                        <option value="0">Inactivo</option>
-                                                    </>
-                                                )
-                                                :(
-                                                    <>
-                                                        <option value="0">Inactivo</option>
-                                                        <option value="1">Activo</option>
-                                                    </>
-                                                )
-                                        }        
-                                    </select>
-                                </div>
-                            </>
-                        ))
-                    }
 
-                </div>
-                <input type="submit" className='btn btn-1 mt-3' value='Guardar'/>
-            </form>
-        </>
-    )
+  /* GUARDA LOS CAMBIOS DE EDICION DEL FACTOR Y REDIRIGE A LA RUTA CAMBIOTRMIATA */
+  
+  const guardar = (e) => {
+    e.preventDefault();
+    let data = {
+      nombre_moneda: document.getElementById("nombre_moneda").value,
+      tipo_factor: document.getElementById("tipo_factor").value,
+      valor_cambio: document.getElementById("valor_cambio").value,
+      estado_cambio: checked,
+    };
 
-}
+    axios
+      .put(process.env.REACT_APP_API_HOST + "cnfFactorCambio/" + params.id, data)
+      .then((response) => {
+        alert("Registro actualizado");
+        history("/administrador/cambiotrmiata");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-export default EditCnfMoneda;
+/* ESTO ES UNA PRUEBA, BORRAR AL TERMINAR */
+
+  const identificarId = (e) => {
+    e.preventDefault();
+    let datab = {
+        nombre_moneda: document.getElementById("nombre_moneda").value,
+        tipo_factor: document.getElementById("tipo_factor").value,
+        valor_cambio: document.getElementById("valor_cambio").value,
+        estado_cambio: checked,
+      };
+
+    const myRegex = /(\d+)(?!.*\d)/;
+    const result = myRegex.exec(location.pathname).join("")
+    console.log(datab)
+  }
+/* ESTO ES UNA PRUEBA, BORRAR AL TERMINAR ^^^^^^^^^^^^^^^^*/
+
+  if (loading) {
+    return (
+      <>
+        <div className="text-center">
+          <div>
+            <LinearProgress />
+          </div>
+          <div>
+            <img src={logo} width="300" alt="" />
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div className="p-2 btn-1 text-center">Editar tipo de Cambio</div>
+      
+      <form className="form-control pt-3" onSubmit={guardar}>
+        <div className="row">
+          <div className="row">
+            <div className="col">
+              <label className="form-label">Moneda</label>
+              <input
+                type="text"
+                id="nombre_moneda"
+                className="form-control"
+                defaultValue='Dolar'
+              />
+            </div>
+            <div className="col">
+              <label className="form-label">Tipo de factor</label>
+              <select id="tipo_factor" className="form-select">
+                <option value="TRM">TRM</option>
+                <option value="IATA">IATA</option>
+              </select>
+            </div>
+          </div>
+          <div className="row mt-3">
+            <div className="col">
+              <label className="form-label">Valor</label>
+              <input
+                type="number"
+                id="valor_cambio"
+                className="form-control"
+                defaultValue='1000'
+              />
+            </div>
+
+            <div className="col d-flex flex-column justify-content-center">
+              <label className="form-label">Estado</label>
+              <FormControlLabel
+                id="estado_cambio"
+                control={<Switch checked={checked} onChange={switchHandler}/>}
+                label={
+                    checked
+                    ? "Activo"
+                    : "Inactivo"
+                }
+              />
+            </div>
+          </div>
+        </div>
+        <input type="submit" className="btn btn-1 mt-3" value="Guardar" />
+        <button className="btn btn-primary ms-3 mt-3" onClick={(e) =>identificarId(e) }>Location</button>
+      </form>
+    </>
+  );
+};
+
+
+export default EditCambioTrmIata;
